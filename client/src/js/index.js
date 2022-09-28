86317e7b3701a9f825e507b760d7012079e3f87c
Ron Dronet <ron@dronetdevdesign.com>
2022-09-28 17:53:19 -0500

    configure iDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,7 +10,10 @@ import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 import DronetLogo from '../images/dronet-logo.png';
 
+import { initdb } from './database';
+
 window.addEventListener('load', function () {
+  initdb();
   document.getElementById('dronet-logo').src = DronetLogo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
