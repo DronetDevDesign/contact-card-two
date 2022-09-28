18c6daccdf98299744bece3e2a37e8777800e2ae
Ron Dronet <ron@dronetdevdesign.com>
2022-09-28 18:23:26 -0500

    configure iDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,11 +10,15 @@ import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 import DronetLogo from '../images/dronet-logo.png';
 
-import { initdb } from './database';
+import { initdb, getDb, postDb } from './database';
 
 window.addEventListener('load', function () {
   initdb();
   document.getElementById('dronet-logo').src = DronetLogo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
+  // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
+  getDb();
+  postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+  getDb();
 });
